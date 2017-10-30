090d2c9eb48f09c0ae25a33b708552802b824400
jabadia <abadiajuancarlos@gmail.com>
2017-10-30 10:49:34 +0100

    a

--- media/top/tests/es.php
+++
@@ -1,9 +0,0 @@
-<?php
-    header("Content-Type: text/event-stream");
-    header("Cache-Control: no-cache");
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Origin: *");
-$valor = "k";
-echo "data: " . $valor . "\n\n";
-    flush();
-?>
